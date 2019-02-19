87630f2b3599c54ebeaabad188eb96e86ab8cc90
moo <msh9303@naver.com>
2019-02-19 21:49:48 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Int64 n1 = 50;
+            int n2;
+            n2 = 100;
+
+            long sum = n1 + n2;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
